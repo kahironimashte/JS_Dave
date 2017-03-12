9e475eb7654b7ccfa628cc17abef0e20100cdd0e
kahironimashte <fabian@Luchs.fritz.box>
2017-03-12 20:07:59 +0100

    Hier mal eine kleine Aufgabe

---
+++ Abschnitt2_1.js
@@ -0,0 +1,15 @@
+# Was wird in der Konsole ausgegeben und warum bzw. wie nennt man das 
+Phänomen?  ->> antwort
+
+b();
+console.log(a);
+
+var a = 'Hello World!';
+
+function b() {
+	console.log('b ausgeführt');
+}
+
+var antwort = '...'
+
+console.log (antwort)
